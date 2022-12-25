024051bf0b743e3bc6e657aefdff7184e4ebb4f5
Sahil Rajput <sahilrajput03@gmail.com>
2022-12-25 21:12:52 +0530

    Add youtube embedded player.

---
+++ youtube-player-embedded/src/main.ts
@@ -0,0 +1,13 @@
+import './style.css'
+
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <div>
+//     <iframe id='movie_player' width="1280" height="720" src="https://www.youtube.com/embed/LDEYsd0XE7Y" title="Bohemia Mashup - Lalit Singh | Tribute To Bohemia" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+//   </div>
+// `
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+  
+  </div>
+`
