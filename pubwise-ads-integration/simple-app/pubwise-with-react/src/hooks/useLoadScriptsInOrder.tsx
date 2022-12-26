079baab122644cd094841558c7b3bb53d5c41da0
Sahil Rajput <sahilrajput03@gmail.com>
2022-12-26 20:47:28 +0530

    Add react exampel of pubwise.

---
+++ pubwise-ads-integration/simple-app/pubwise-with-react/src/hooks/useLoadScriptsInOrder.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+const useLoadScriptsInOrder = (URLs: string[]) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function main() {
+      try {
+        // Sequential Loading of scripts
+        for (const url of URLs) {
+          await new Promise((resolve) => {
+            const tag = document.createElement("script");
+            // tag.async = true;
+            tag.src = url;
+            const body = document.getElementsByTagName("body")[0];
+            body.appendChild(tag);
+
+            tag.addEventListener("load", resolve);
+          });
+        }
+        setIsLoaded(true);
+      } catch (error: any) {
+        console.error("Slasher LOG: (failed to load PubWise scipts):", {
+          name: error.name,
+          message: error.message,
+          stack: error.stack,
+        });
+      }
+    }
+
+    main();
+  }, []);
+
+  return isLoaded;
+};
+
+export default useLoadScriptsInOrder;
