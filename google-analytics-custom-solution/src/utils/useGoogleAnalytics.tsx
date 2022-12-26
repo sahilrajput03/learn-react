ac9c236778a842f9b2426c1dee984eb4e203d427
Sahil Rajput <sahilrajput03@gmail.com>
2022-12-27 00:51:28 +0530

    Update code.

--- google-analytics-custom-solution/src/utils/useGoogleAnalytics.tsx
+++ google-analytics-custom-solution/src/utils/useGoogleAnalytics.tsx
@@ -1,6 +1,6 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import useScript from './useScript';
+import { useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
+import useScript from "./useScript";
 
 declare global {
   interface Window {
@@ -11,6 +11,7 @@ declare global {
 const useGoogleAnalytics = (analyticsId: string) => {
   const location = useLocation();
   const isLoaded = useScript(`https://www.googletagmanager.com/gtag/js?id=${analyticsId}`);
+  const previousPathRef = useRef<string>();
 
   const { pathname, search, hash } = location;
 
@@ -23,12 +24,17 @@ const useGoogleAnalytics = (analyticsId: string) => {
       // eslint-disable-next-line prefer-rest-params
       window.dataLayer.push(arguments);
     }
-    gtag('js', new Date()); // necessary
-    gtag('config', analyticsId); // necessary
 
-    // LEARN: Please check file `indexVanilla.html` file to know how amazing and flexible this API is. Thanks.
-    gtag('event', 'page_view', {
-      page_location: pathname + search + hash,
+    const path = pathname + search + hash;
+    if (previousPathRef.current === path) return;
+    previousPathRef.current = path;
+
+    gtag("js", new Date()); // necessary
+    gtag("config", analyticsId); // necessary
+
+    // console.log('event sent for path?', path); // USE THIS TO TEST THE ACTUALY SENDING OF EVENT.
+    gtag("event", "page_view", {
+      page_location: path,
       page_title: document.title,
     });
   }, [location, isLoaded]);
